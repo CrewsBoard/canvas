ee17850194dea7d2a229be5c7b67fbb674ac03a8
Anik Sarker <sarkerjr@gmail.com>
2025-04-28 18:41:48 +0600

    chore: setup tailwindcss and shadcn

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,34 +1,35 @@
-import {defineConfig} from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-import {fileURLToPath} from 'url';
-import {dirname, resolve} from 'path';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+import { defineConfig } from 'vite';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            '@': resolve(__dirname, './src'),
-        },
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
     },
-    server: {
-        proxy: {
-            '/api': {
-                target: 'http://localhost:8001',
-                changeOrigin: true,
-                rewrite: path => path.replace(/^\/api/, '')
-            },
-            // @todo need to test this with the custom node
-            '/canvas-custom-node': {
-                target: 'http://localhost:3001',
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/canvas-custom-node/, '')
-            }
-        },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8001',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, ''),
+      },
+      // @todo need to test this with the custom node
+      '/canvas-custom-node': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/canvas-custom-node/, ''),
+      },
     },
-    optimizeDeps: {
-        include: ['@xyflow/react']
-    }
+  },
+  optimizeDeps: {
+    include: ['@xyflow/react'],
+  },
 });
